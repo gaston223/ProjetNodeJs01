63177c22071285baf06ffe2c71b07adde619f31d
unknown <g.coulibalyy@gmail.com>
2018-05-28 15:48:30 +0200

    Version avec Addition et multiplication

--- test.js
+++ test.js
@@ -1,12 +1,29 @@
 // Import des modules
-const module1 = require('module1');
 const http = require('http');
+const calculatrice = require('calculatrice');
+
 
 //Création du serveur HTTP
 let server = http.createServer((request, response)=>{
     console.log('Requete vers le serveur');
-    response.write('<h1>Hello</h1>');
-    response.write('<h2>Fine</h2>');
+    response.write('<meta charset="utf-8" />')
+    response.write('<h1>Bonjour</h1>');
+    response.write('<h2>Bien?</h2>');
+
+    let resultat = calculatrice.addition(105, 134);
+    response.write(`<p>Mon résultat: "${resultat}"</p>`);
+
+    resultat=calculatrice.multiplication(11, 32);
+    response.write(`<p>Multiplication: "${resultat}"</p>`);
+
+    //utilisation de la fonction principale de la calculatrice
+    resultat= calculatrice('addition', 5 ,17);
+    response.write(`<p>Res Addition: "${resultat}"</p>`)
+
+    resultat= calculatrice('multiplication', 4 ,9);
+    response.write(`<p>Res multiplication: "${resultat}"</p>`)
+
     response.end();
+
 });
 server.listen(3001);
